577cec0c2e4e60402726d84c01f41998f5789cfc
Nikolas Sigouin <nik.sigouin@gmail.com>
2019-12-23 02:34:31 -0500

    Added extended address for user input

--- index.js
+++ index.js
@@ -8,18 +8,19 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function (socket) {
+    // Gets unique user ID for connected user
     var user = socket.id;
     console.log(user + " connected.");
 
     const client = new Client('127.0.0.1', 3334);
     // Gets the input from the webpage and sends it through OSC
-    socket.on('change:interval', function(data){
+    socket.on('change:interval', function(val, name){
         // Converts the input into an int
-        const input = Number(data);
+        const value = Number(val);
         // Prepares the Message to ship to OSC
-        var msg = new Message('/slider', input)
+        var msg = new Message('/' + user + '/' + name, value);
         client.send(msg);
-        console.log('message: ' + input)
+        console.log(msg);
     });
 
     socket.on("disconnect", function () {
